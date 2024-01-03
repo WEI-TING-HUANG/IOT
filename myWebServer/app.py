cab135598abca8efe415e723db83b86b03d7af18
WEI-TING-HUANG <155205404+WEI-TING-HUANG@users.noreply.github.com>
2024-01-03 23:34:04 +0800

    Update app.py

--- myWebServer/app.py
+++ myWebServer/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, session
 from camera import VideoCamera
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+app.secret_key='sjehfjeefrjewth43u' 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37, GPIO.OUT)         #LED output pin
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html' , sound_flag=session.get('soundFlag'))
 
 def gen(camera):
     while True:
@@ -18,5 +24,16 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/sound_feed')
+def sound_feed():
+    if session.get('soundFlag') == 0:
+        session['soundFlag'] = 1
+    else:
+        session['soundFlag'] = 0
+    
+    GPIO.output(37, session.get('soundFlag'))
+    return render_template('index.html' , sound_flag=session.get('soundFlag'))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80,debug=True)
+    session['soundFlag'] = 0
